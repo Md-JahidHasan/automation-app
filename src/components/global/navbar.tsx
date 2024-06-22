509a064ba70609c95f9268c6eb242cdb6fb834d6
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2024-06-23 00:20:30 +0600

    Config file update

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,4 +1,4 @@
-import { MenuIcon } from 'lucide-react';
+import { MenuIcon, MenuSquare } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -6,7 +6,7 @@ type Props = {}
 
 const Navbar =  async (props: Props) => {
   return (
-    <header className='fixed left-0 right-0 top-0 px-4 py-4 bg-white/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
+    <header className='fixed left-0 right-0 top-0 px-4 py-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
       <aside className='flex item-center gap-[2px]'>
         <p className='text-3xl font-bold'>Fu</p>
         <Image
@@ -67,13 +67,17 @@ const Navbar =  async (props: Props) => {
         <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]'/>
 
         <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-900 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
-        {true ? 'Dashboard': 'Get Started'}
+        
+            {/* Wire up user */}
+            
+            {true ? 'Dashboard' : 'Get Started'}
 
         </span>
 
         </Link>
     
-        <MenuIcon className='md:hidden'/>
+        <MenuIcon className='lg:hidden' />
+
       </aside>
 
     </header>
