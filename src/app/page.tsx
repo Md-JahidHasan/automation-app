ecf90760e8541cdbf4cfee0692f76d4b1f88c826
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2024-06-24 01:51:19 +0600

    Container Scroll section add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import Navbar from "@/components/global/navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 
@@ -10,8 +12,26 @@ export default function Home() {
       
       <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
 
-        <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col">
+        {/* <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col">
 
+        </div> */}
+        <div className=" absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]">
+          hg
+        </div>
+
+        <div className=" flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll titleComponent={
+            <div className=" flex items-center justify-center flex-col">
+              <Button
+                size={'lg'}
+                className=" p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+              >
+                <span className=" bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600 md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                  Start For free Today
+                </span>
+              </Button>
+            </div>
+          }></ContainerScroll>
         </div>
 
 
