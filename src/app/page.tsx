2133aaa609f94ab67a82dcdb0f44058dc76b47f1
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2024-06-23 19:11:15 +0600

    Navbar Settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,16 @@ export default function Home() {
   return (
     <main className="">
       
-      <Navbar/>
+      <Navbar />
+      
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
+
+        <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col">
+
+        </div>
+
+
+      </section>
      
 
       
